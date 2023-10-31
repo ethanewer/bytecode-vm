cf350f6dcd0cd5b47b218db657c060a95a40f164
Ethan Ewer <eewer@wisc.edu>
2023-10-31 00:43:03 -0500

    small changes

--- value.cpp
+++ value.cpp
@@ -48,11 +48,7 @@ void printValue(Value value) {
 }
 
 bool valuesEqual(Value a, Value b) {
-
 #ifdef NAN_BOXING
-  if (IS_NUMBER(a) && IS_NUMBER(b)) {
-    return AS_NUMBER(a) == AS_NUMBER(b);
-  }
   return a == b;
 #else
   if (a.type != b.type) return false;
@@ -63,6 +59,7 @@ bool valuesEqual(Value a, Value b) {
     case VAL_OBJ:    return AS_OBJ(a) == AS_OBJ(b);
     default:         return false; 
   }
+
 #endif
 }
 
